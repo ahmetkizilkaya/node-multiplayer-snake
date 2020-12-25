456bbf9b73e74ce1dcff33b9adde7520279758ea
Ahmet KIZILKAYA <ahmetkizilkaya43@gmail.com>
2020-12-25 21:50:55 +0300

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
        serverUrl: 'http://192.168.144.8:9000',
+       token : "8f9496247cba72c534e9e2a6289dfb760bf409e0",
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
